eab580683aaf89e061d7a4ebf8d340008cc4f052
usuario <usuario@LAPTOP-OPHQGTON>
2016-09-27 14:55:33 +0200

    Clean Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,28 +6,28 @@ MAINTAINER solutions@nfq.com
 RUN sudo yum install -y wget
 
 # Variables de entorno
-ENV JAVA_HOME /home/solutions/app/java
-ENV JAVA_TOOL_OPTIONS -Dfile.encoding=UTF8
-ENV CATALINA_HOME /home/solutions/app/tomcat
-ENV PATH $PATH:$JAVA_HOME/bin:$CATALINA_HOME/bin
-ENV JAVA_VERSION=7u80
-ENV TOMCAT_VERSION=7.0.70
+ENV JAVA_HOME=/home/solutions/app/java && \
+	JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8 && \
+	CATALINA_HOME=/home/solutions/app/tomcat && \
+	PATH=$PATH:$JAVA_HOME/bin:$CATALINA_HOME/bin && \
+	JAVA_VERSION=7u80 && \
+	TOMCAT_VERSION=7.0.70
 
 # Modificacion para solutions
-COPY index.html /home/solutions/
-COPY solutions.png /home/solutions/
-COPY setenv.sh /home/solutions/
-RUN sudo chown solutions:nfq /home/solutions/setenv.sh
-RUN sudo chmod 777 /home/solutions/setenv.sh
-RUN sudo chmod a+x /home/solutions/setenv.sh
-RUN sudo sed -i -e 's/\r$//' /home/solutions/setenv.sh
+COPY index.html /home/solutions/ && \
+	solutions.png /home/solutions/ && \
+	setenv.sh /home/solutions/
+RUN sudo chown solutions:nfq /home/solutions/setenv.sh && \
+	chmod 777 /home/solutions/setenv.sh && \
+	chmod a+x /home/solutions/setenv.sh && \
+	sed -i -e 's/\r$//' /home/solutions/setenv.sh
 
 # Script de arranque
 COPY tomcat.sh /home/solutions/
-RUN sudo chown solutions:nfq /home/solutions/tomcat.sh
-RUN sudo chmod 777 /home/solutions/tomcat.sh
-RUN sudo chmod a+x /home/solutions/tomcat.sh
-RUN sudo sed -i -e 's/\r$//' /home/solutions/tomcat.sh
+RUN sudo chown solutions:nfq /home/solutions/tomcat.sh && \
+	chmod 777 /home/solutions/tomcat.sh && \
+	chmod a+x /home/solutions/tomcat.sh && \
+	sed -i -e 's/\r$//' /home/solutions/tomcat.sh
 
 # Volumenes para el tomcat
 VOLUME /home/solutions/app
@@ -35,7 +35,7 @@ VOLUME /home/solutions/app
 # Puerto de salida del tomcat
 EXPOSE 8080
 
-# Copy supervisor file
+# Configuracion supervisor
 COPY supervisord.conf /etc/supervisord.conf
 
 CMD ["/usr/bin/supervisord"]
--- tomcat.sh
+++ tomcat.sh
@@ -2,26 +2,26 @@
 
 echo Instalando java ...
 if [ ! -f /home/solutions/app/java/bin/java ]; then
-	sudo wget -P /home/solutions/app/ --no-cookies --no-check-certificate --header \
+	wget -P /home/solutions/app/ --no-cookies --no-check-certificate --header \
     	"Cookie: oraclelicense=accept-securebackup-cookie" \
     	"http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}-b15/jdk-${JAVA_VERSION}-linux-x64.tar.gz"
-	sudo tar -xvzf /home/solutions/app/jdk-${JAVA_VERSION}-linux-x64.tar.gz -C /home/solutions/app/
-	sudo chown -R solutions:nfq $(ls -d /home/solutions/app/jdk*/)
-	sudo chmod -R 777 $(ls -d /home/solutions/app/jdk*/)
-	sudo ln -sf $(ls -d /home/solutions/app/jdk*/) /home/solutions/app/java
+	tar -xvzf /home/solutions/app/jdk-${JAVA_VERSION}-linux-x64.tar.gz -C /home/solutions/app/
+	#chown -R solutions:nfq $(ls -d /home/solutions/app/jdk*/)
+	chmod -R 777 $(ls -d /home/solutions/app/jdk*/)
+	ln -sf $(ls -d /home/solutions/app/jdk*/) /home/solutions/app/java
 fi
 
 echo Instalando tomcat ...
 if [ ! -f /home/solutions/app/tomcat/bin/catalina.sh ]; then
-	sudo wget -P /home/solutions/app/ "http://archive.apache.org/dist/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
-	sudo tar -xvzf /home/solutions/app/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /home/solutions/app/
-	sudo chown -R solutions:nfq $(ls -d /home/solutions/app/apache-tomcat*/)
-	sudo chmod -R 777 $(ls -d /home/solutions/app/apache-tomcat*/)
-	sudo ln -sf $(ls -d /home/solutions/app/apache-tomcat*/) /home/solutions/app/tomcat
-	sudo chmod a+x /home/solutions/app/tomcat/bin/catalina.sh
-	sudo cp /home/solutions/index.html /home/solutions/app/tomcat/webapps/ROOT/
-    sudo cp /home/solutions/solutions.png /home/solutions/app/tomcat/webapps/ROOT/
-    sudo mv /home/solutions/app/tomcat/webapps/ROOT/index.jsp /home/solutions/app/tomcat/webapps/ROOT/index.jsp.bak
+	wget -P /home/solutions/app/ "http://archive.apache.org/dist/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
+	tar -xvzf /home/solutions/app/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /home/solutions/app/
+	#chown -R solutions:nfq $(ls -d /home/solutions/app/apache-tomcat*/)
+	chmod -R 777 $(ls -d /home/solutions/app/apache-tomcat*/)
+	ln -sf $(ls -d /home/solutions/app/apache-tomcat*/) /home/solutions/app/tomcat
+	chmod a+x /home/solutions/app/tomcat/bin/catalina.sh
+	cp /home/solutions/index.html /home/solutions/app/tomcat/webapps/ROOT/
+    cp /home/solutions/solutions.png /home/solutions/app/tomcat/webapps/ROOT/
+    mv /home/solutions/app/tomcat/webapps/ROOT/index.jsp /home/solutions/app/tomcat/webapps/ROOT/index.jsp.bak
 fi
 
 echo Configurando tomcat ...
